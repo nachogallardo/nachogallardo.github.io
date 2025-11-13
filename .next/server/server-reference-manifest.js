10f19da37a6286ed90c2c8c2dc8331b025f98230
nachogallardo <nacho-gallardo-sanchez@hotmail.com>
2025-11-13 21:53:36 +0100

    Add production build and fix gitignore

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2N+XLtjs3ftFmQGpnPRvJDf0Jci5QbvyJU19+7LJYyo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[{"source":"/nuevo-portfolio//_next/:path+","destination":"/nuevo-portfolio/_next/:path+"}],"fallback":[]},"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"sortedPages":["/_app","/_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
